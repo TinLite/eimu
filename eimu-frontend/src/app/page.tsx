3e03d915fc30a0507f64a91c8ae30cd86b426b71
TuanVux <tuanvukg2703@gmail.com>
2024-03-25 19:14:03 +0700

    svg=>jsx; item center=>start; match_btn-sidelist,fix-location

--- eimu-frontend/src/app/page.tsx
+++ eimu-frontend/src/app/page.tsx
@@ -4,7 +4,8 @@ import SideList from "@/app/components/SideList";
 import Link from "next/link";
 
 export default async function Home() {
-  const new_movie_list = await (await fetch("http://51.79.144.118:12594/movie")).json()
+  const res = await fetch("http://51.79.144.118:12594/movie")
+  const new_movie_list = await res.json();
   return (
     <main>
       <div className="relative overflow-hidden h-[450px]">
