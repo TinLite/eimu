31d6098c70929b0eda7a50a8c13fbc0df01e1552
TuanVux <tuanvukg2703@gmail.com>
2024-03-30 23:47:37 +0700

    fix_import

--- eimu-frontend/src/app/(main)/layout.tsx
+++ eimu-frontend/src/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 
