4a890bcec2b2a4a46c8a472cf623a672c93dc8e5
TuanVux <tuanvukg2703@gmail.com>
2024-03-31 11:44:22 +0700

    up_icon_login

--- eimu-frontend/src/app/components/Navbar.tsx
+++ eimu-frontend/src/app/components/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar: React.FC = () => {
                     </Link>
                     <Link href={"/login"} className="px-4 py-2 rounded-md bg-blue-50 text-[#006FEE] flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
                         </svg>
                         <span className="ml-1">Đăng nhập</span></Link>
                 </div>
