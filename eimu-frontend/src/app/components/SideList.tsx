db6d07895369c1f2b051c9c133c8f7d20c71ee34
TinLite <37470201+TinLite@users.noreply.github.com>
2024-04-02 11:57:39 +0700

    frontend: fix navigate button

--- eimu-frontend/src/app/components/SideList.tsx
+++ eimu-frontend/src/app/components/SideList.tsx
@@ -17,13 +17,11 @@ export default async function SideList({
             <div><Link href={link} className=" hover:text-red-400">Xem thêm</Link></div>
         </div>
         <div className="flex px-16 gap-5 w-screen overflow-x-auto items-start snap-x">
-            <div className="flex flex-wrap gap-4 items-center border-2 rounded-full hover:bg-white hover:text-black absolute left-5 my-32">
-                <Button color="primary" variant="ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                    </svg>
-                </Button>
-            </div>
+            <button className="flex flex-wrap gap-4 items-center border-2 rounded-full hover:bg-white hover:text-black absolute left-5 my-32">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                </svg>
+            </button>
             {data.map((entry) => (
                 <div key={entry.id}>
                     <a href="#">
@@ -43,13 +41,11 @@ export default async function SideList({
                 </div>
             ))
             }
-            <div className="flex flex-wrap gap-4 items-center border-2 rounded-full hover:bg-white hover:text-black absolute right-5 my-32">
-                <Button color="primary" variant="ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
-                </Button>
-            </div>
+            <button className="flex flex-wrap gap-4 items-center border-2 rounded-full hover:bg-white hover:text-black absolute right-5 my-32">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+            </button>
         </div>
     </div>
 }
