7ec33d37f557a0606a6d0b9b7ad8e8279a43aa58
TuanVux <tuanvukg2703@gmail.com>
2024-04-02 20:19:36 +0700

    fixed-css

--- eimu-frontend/src/app/(main)/movie/[movieId]/[[...epId]]/page.tsx
+++ eimu-frontend/src/app/(main)/movie/[movieId]/[[...epId]]/page.tsx
@@ -5,13 +5,13 @@ import MovieInfo from '@/app/components/MovieInfo';
 import MoviePlayer from '@/app/components/MoviePlayer';
 import Comment from '@/app/components/Comment';
 
-export default async function Detail({params} : {params : {movieId: string, epId?: string}}) {
+export default async function Detail({ params }: { params: { movieId: string, epId?: string } }) {
     const movieId = params.movieId
     const epId = params.epId ?? "1"
     const movieDetail = await getMovieDetail(movieId)
     const new_movie_list = await getLatestMovies()
     return (
-        <div className='text-gray-200 max-w-screen-xl mx-auto mt-4'>
+        <div className='text-gray-200 max-w-screen-xl mx-auto'>
             <MovieInfo movie={movieDetail} />
             <MoviePlayer />
             <SideList title="Đề xuất cho bạn" link="#" data={new_movie_list.items} />
