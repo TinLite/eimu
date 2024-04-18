248808d7fdf4b7af380c6e579514bb88882971ef
TuanVux <tuanvukg2703@gmail.com>
2024-04-18 14:22:46 +0700

    follow-history-table-list-NEXTUI

--- eimu-frontend/src/app/user/follows/page.tsx
+++ eimu-frontend/src/app/user/follows/page.tsx
@@ -3,14 +3,15 @@ import MenuAccY from '@/app/components/Sidebar'
 import { Button } from '@nextui-org/react'
 import Link from 'next/link'
 import React from 'react'
-import FavoriteTableList from '@/app/components/FavoriteTableList'
+import FavoriteTableList from '@/app/components/FollowTableList'
+import FollowTableList from '@/app/components/FollowTableList'
 
-export default async function Favorite() {
+export default async function Follow() {
   const new_movie_list = await (await fetch("http://51.79.144.118:12594/movie")).json()
   return (
     <div className='flex text-white'>
       <MenuAccY />
-      <FavoriteTableList />
+      <FollowTableList />
     </div>
   )
 }
