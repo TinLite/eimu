3d33b51dde1d40e3b2182c4dd3b85f3ad59c9e35
TuanVux <tuanvukg2703@gmail.com>
2024-04-18 14:40:14 +0700

    userdetail-NEXTUI-Modal

--- eimu-frontend/src/app/user/detail/page.tsx
+++ eimu-frontend/src/app/user/detail/page.tsx
@@ -1,10 +1,12 @@
+'use client'
 import MenuAccY from '@/app/components/Sidebar'
-import { Avatar, Button, useDisclosure, Input } from '@nextui-org/react'
 import React from 'react'
-import { Select, SelectItem } from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Avatar, Input } from "@nextui-org/react";
+import { RadioGroup, Radio } from "@nextui-org/react";
 
 
-export default function page() {
+export default function UserDetail() {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
     return (
         <div className='flex text-white'>
             <MenuAccY />
@@ -17,49 +19,54 @@ export default function page() {
                                 <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-20 h-20 text-large mr-7" />
                                 <div className='font-bold text-2xl'>Tiến</div>
                             </div>
-                            <label htmlFor="my_modal_6" className="btn">Chỉnh sửa</label>
-
-                            {/* Put this part before </body> tag */}
-                            <input type="checkbox" id="my_modal_6" className="modal-toggle" />
-                            <div className="modal" role="dialog">
-                                <div className="modal-box">
-                                    <h3 className="text-lg mb-5">Chỉnh sửa thông tin cá nhân</h3>
-                                    <form action="">
-                                        <div className='mb-5'>
-                                            <label className='mr-5' htmlFor="avatar">Tải ảnh đại diện: </label>
-                                            <input type="file" name='avatar' id='avatar' className="file-input w-full max-w-xs" />
-                                        </div>
-                                        <div className=" w-full mb-5">
-                                            <Input type="text" label="Họ tên" placeholder="Lê Quang Tiến" />
-                                        </div>
-                                        <div className=" w-full mb-5">
-                                            <Input type="date" label="Ngày sinh" placeholder="" />
-                                        </div>
-                                        <div className=" w-full mb-5">
-                                            <label className="form-control w-full">
-                                                <div className="label">
-                                                    <span className="label-text text-white">Giới tính</span>
-                                                </div>
-                                                <select className="select select-bordered">
-                                                    <option disabled selected>Chọn giới tính</option>
-                                                    <option>Nam</option>
-                                                    <option>Nữ</option>
-                                                </select>
-                                            </label>
-                                        </div>
-                                        <div className=" w-full mb-5">
-                                            <Input type="Email" label="Email" placeholder="LeQuangTien@gmail.com" />
-                                        </div>
-                                        <div className=" w-full mb-5">
-                                            <Input type="tel" label="Số điện thoại" placeholder="0935964574" />
-                                        </div>
-                                    </form>
-                                    <div className="modal-action flex items-center">
-                                        <label htmlFor="my_modal_6" className="btn">Hủy</label>
-                                        <label className="btn">Chỉnh sửa</label>
-                                    </div>
-                                </div>
-                            </div>
+                            <>
+                                <Button onPress={onOpen}>Chỉnh sửa</Button>
+                                <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                                    <ModalContent>
+                                        {(onClose) => (
+                                            <>
+                                                <ModalHeader className="flex flex-col gap-1">Chỉnh sửa thông tin cá nhân</ModalHeader>
+                                                <ModalBody>
+                                                    <form action="" method='POST'>
+                                                        <div className='mb-5'>
+                                                            <label className='mr-5' htmlFor="avatar">Tải ảnh đại diện: </label>
+                                                            <input type="file" name='avatar' id='avatar' className=" w-full max-w-xs" />
+                                                        </div>
+                                                        <div className=" w-full mb-5">
+                                                            <Input type="text" label="Họ tên" placeholder="Lê Quang Tiến" />
+                                                        </div>
+                                                        <div className=" w-full mb-5">
+                                                            <Input type="date" label="Ngày sinh" placeholder="" />
+                                                        </div>
+                                                        <RadioGroup
+                                                            label="Chọn giới tính"
+                                                            orientation="horizontal"
+                                                            className='mb-5'
+                                                        >
+                                                            <Radio value="nam">Nam</Radio>
+                                                            <Radio value="nu">Nữ</Radio>
+                                                        </RadioGroup>
+                                                        <div className=" w-full mb-5">
+                                                            <Input type="Email" label="Email" placeholder="LeQuangTien@gmail.com" />
+                                                        </div>
+                                                        <div className=" w-full mb-5">
+                                                            <Input type="tel" label="Số điện thoại" placeholder="0935964574" />
+                                                        </div>
+                                                    </form>
+                                                </ModalBody>
+                                                <ModalFooter>
+                                                    <Button color="danger" variant="light" onPress={onClose}>
+                                                        Close
+                                                    </Button>
+                                                    <Button color="primary" onPress={onClose}>
+                                                        Chỉnh sửa
+                                                    </Button>
+                                                </ModalFooter>
+                                            </>
+                                        )}
+                                    </ModalContent>
+                                </Modal>
+                            </>
                         </div>
                         <div className='flex border-b-2 py-5'>
                             <div className='mr-5 '>Họ tên:</div>
