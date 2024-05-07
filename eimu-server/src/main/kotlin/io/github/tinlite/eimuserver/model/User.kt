fbf55bf1549b8d82cda1b8d8550da136912f8b04
QuangTeoo <116084622+QuangTeoo@users.noreply.github.com>
2024-05-07 13:19:36 +0700

    chỉnh sữa show user

--- eimu-server/src/main/kotlin/io/github/tinlite/eimuserver/model/User.kt
+++ eimu-server/src/main/kotlin/io/github/tinlite/eimuserver/model/User.kt
@@ -1,5 +1,6 @@
 package io.github.tinlite.eimuserver.model
 
+import com.fasterxml.jackson.annotation.JsonView
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -7,15 +8,17 @@ import java.util.*
 
 @Document("user")
 data class User(
-    @Id val id: ObjectId?,
-    var name: String,
-    var dateOfBirth: Date?,
-    var phone: String?,
-    var email: String?,
+    @JsonView(UserDetail::class) @Id val id: ObjectId?,
+    @JsonView(UserDetail::class) var name: String,
+    @JsonView(UserDetail::class) var dateOfBirth: Date?,
+    @JsonView(UserDetail::class) var phone: String?,
+    @JsonView(UserDetail::class) var email: String?,
     var follows: MutableList<String>?,
-    var role: String,
+    @JsonView(UserDetail::class) var role: String,
     val hashedPassword: String
-)
+) {
+    interface UserDetail
+}
 
 data class UserLoginDetail(
     @Id val id: ObjectId,
