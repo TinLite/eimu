63d4764bf04ad0be09a4521d43b24c5062dac51a
TinLite <37470201+TinLite@users.noreply.github.com>
2024-06-10 15:07:07 +0700

    frontend: Thay "Trở về trang chủ" thành "Trở về trang trước"

--- eimu-frontend/src/app/not-found.tsx
+++ eimu-frontend/src/app/not-found.tsx
@@ -1,7 +1,6 @@
 'use client';
 import '@/app/globals.css';
 import { Inter } from 'next/font/google';
-import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +13,7 @@ export default function NotFound() {
                         <h1 className='text-9xl text-center font-black'>404</h1>
                         <h2 className='text-center uppercase bg-black font-bold absolute top-1/2 w-full -translate-y-1/2'>Trang không tồn tại</h2>
                     </div>
-                    <Link href='/' className='btn btn-ghost btn-block mt-4'>Trở về trang chủ</Link>
+                    <button onClick={() => history.back()} className='btn btn-ghost btn-block mt-4'>Trở về trang trước</button>
                 </div>
             </div>
         </body>
