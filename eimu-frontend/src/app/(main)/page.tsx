eb127729ef43b98858585dccd52fa420b34af154
TuanVux <tuanvukg2703@gmail.com>
2024-06-14 11:38:47 +0700

    hiển thị tổng điểm đánh giá trên carousel
    Co-authored-by: Vinh <TinLite@users.noreply.github.com>

--- eimu-frontend/src/app/(main)/page.tsx
+++ eimu-frontend/src/app/(main)/page.tsx
@@ -12,10 +12,18 @@ export default async function Home() {
   const anime_movie_list = await getLatestMoviesByTag("45c48cce2e2d7fbdea1afc51c7c6ad26,d2ddea18f00665ce8623e36bd4e3c7c5")
   const chinese_movie_list = await getLatestMoviesByTag("c0c7c76d30bd3dcaefc96f40275bdc0a")
   const tag_list = await getAllTag()
-  
+  const arr_id = await Promise.all(new_movie_list.items.map((v) => getRate(v.id)))
+
+  const mov = new_movie_list.items.map((v) => {
+    return {
+      ...v,
+      rate: arr_id.find(v2 => v.id == v2.movieId)
+    }
+  })
+
   return (
     <div className="text-white">
-      <Carousel items={new_movie_list.items} tagList={tag_list}/>
+      <Carousel items={mov} tagList={tag_list} />
       <SideList title="Vừa cập nhật!" link="/genres" data={new_movie_list.items} />
       <SideList title="Phim bộ" link="/genres" data={tv_movie_list.items} />
       <SideList title="Phim lẻ" link="/genres" data={theater_movie_list.items} />
--- eimu-frontend/src/app/model/MovieModels.ts
+++ eimu-frontend/src/app/model/MovieModels.ts
@@ -1,4 +1,4 @@
-export interface MovieListEntry {
+export type MovieListEntry = {
     id: string,
     name: string,
     originalName?: string,
